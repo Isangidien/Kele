29a5568940ce198c39c4dbccb97301e4b24f4f36
isangidien <i.inessa.miller@gmail.com>
2017-08-14 21:58:28 -0400

    complete: fixed irb issue

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,15 +1,21 @@
 require 'httparty'
 require 'json'
+require 'kele'
 
 class Kele
   include HTTParty
-    base_url "https://www.bloc.io/api/v1"
+    base_uri "https://www.bloc.io/api/v1"
 
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
-    raise "invalid email or password" if response.code != 200
+    raise InvalidError.new() if response.code != 200
     @auth_token = response["auth_token"]
   end
 
+  def get_me
+    url = "https://www.bloc.io/api/v1/users/me"
+    response = self.class.get(url, headers: {"authorization" => @auth_token})
+    body = JSON.parse(response.body)
+  end
 
 end
