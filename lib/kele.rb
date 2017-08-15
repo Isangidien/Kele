1d725fa4f8a7db70585bc4d37cb97461b6d8faa8
isangidien <i.inessa.miller@gmail.com>
2017-08-15 15:14:47 -0400

    mentor availability: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,7 +8,7 @@ class Kele
 
   def initialize(email, password)
     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
-    raise InvalidError.new() if response.code != 200
+    #raise InvalidError.new() if response.code != 200
     @auth_token = response["auth_token"]
   end
 
@@ -18,4 +18,9 @@ class Kele
     body = JSON.parse(response.body)
   end
 
+  def get_mentor_availabiltiy(id)
+    mentor_url = "https://www.bloc.io/api/v1/mentors/#{id}/student_availability"
+    response = self.class.get(mentor_url, headers: {"content_type" => "application/jason", "authorization" => @auth_token})
+    body = JSON.parse(response.body)
+  end
 end
