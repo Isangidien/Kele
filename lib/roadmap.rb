8527e7f4b90bcf764fb28c343d8a4fafb3cd6920
isangidien <i.inessa.miller@gmail.com>
2017-08-16 16:49:37 -0400

    messages available for view

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -3,12 +3,13 @@ module Roadmap
   def get_roadmap(id)
     roadmap_url = "https://www.bloc.io/api/v1/roadmaps/#{id}"
     response = self.class.get(roadmap_url, headers: {"content_type" => "application/json", "authorization" => @auth_token})
-    JSON.parse(responce.body)
+    body = JSON.parse(responce.body)
   end
 
   def get_checkpoint(id)
     checkpoint_url = "https://www.bloc.io/api/v1/checkpoints/#{id}"
     response = self.class.get(checkpoint_url, headers: {"content_type" => "application/json", "authorization" => @auth_token})
-    JSON.parse(response.body)
+    body = JSON.parse(response.body)
   end
+
 end
